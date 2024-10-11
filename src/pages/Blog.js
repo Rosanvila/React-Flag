f67199c51c4633a63899e97381d7830d8d47a6c9
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-10-11 12:46:05 +0200

    textarea condition

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,18 +2,37 @@ import React from 'react';
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
-const Blog = () => (
-    <div className="blog-container">
-        <Logo />
-        <Navigation />
-        <h1>Blog</h1>
-        <form>
-            <input type="text" placeholder="Name" />
-            <textarea placeholder="Message"></textarea>
-            <input type="submit" value="Submit" />
-        </form>
-        <ul></ul>
-    </div>
-);
+const Blog = () => {
+    const [content, setContent] = React.useState("");
+    const [error, setError] = React.useState(false);
+    const handle = (ev) => {
+        ev.preventDefault();
+
+        if (content.length < 140) {
+            setError(true);
+        } else {
+            setError(false);
+            // Send the message to the server
+        }
+    };
+
+    return (
+        <div className="blog-container">
+            <Logo/>
+            <Navigation/>
+            <h1>Blog</h1>
+            <form onSubmit={(ev) => handle(ev)}>
+                <input type="text" placeholder="Name"/>
+                <textarea placeholder="Message" style={{border: error ? "1px solid red" : "1px solid #67dafb"}}
+                          onChange={(ev) => setContent(ev.target.value)}></textarea>
+                <p>
+                    {error && "Your message is too short, it should be at least 140 characters long."}
+                </p>
+                <input type="submit" value="Submit"/>
+            </form>
+            <ul></ul>
+        </div>
+    );
+};
 
 export default Blog;
